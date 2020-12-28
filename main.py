01d218e97e43e5100bc4cf83eeb43e3a3f30314f
Will <Wotty@users.noreply.github.com>
2020-12-28 15:57:34 +0000

    Tutorial 9
    Python & kv
    
    - Learnt how to use screenmanager and screeens. Transisitioned between multiple screens using "root.manager.transition"
    
    - Used if logic in kv to go to next screen based on a password TextInput field.

--- main.py
+++ main.py
@@ -1,39 +1,24 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from kivy.graphics import Rectangle
-from kivy.graphics import Color
-from kivy.graphics import Line
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
 
-class Touch(Widget):
+class MainWindow(Screen):
+    pass
 
-    def __init__(self,**kwargs):
-        super(Touch, self).__init__(**kwargs)
+class SecondWindow(Screen):
+    pass
 
-        with self.canvas:
-            Color(1,0,0,.5 ,mode='rgba')
-            self.rect = Rectangle(pos=(0,0), size=(50,50))
+class WindowManager(ScreenManager):
+    pass
 
-            Color(1,0,1,.5 ,mode='rgba')
-            Line(points=(20,30,400,500,60,500))
-
-
-
-    def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Down", touch)
-
-
-    def on_touch_move(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Move", touch)
+kv = Builder.load_file("my.kv")
 
 
 class MyApp(App):
     def build(self):
-            return Touch()
+            return kv
 
 if __name__== "__main__":
     MyApp().run()
